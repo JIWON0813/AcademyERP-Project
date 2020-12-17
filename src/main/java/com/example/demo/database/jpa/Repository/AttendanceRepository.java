1808d7080018cb00f50f263d2adbc18558ba4b14
parknnna <af777888@naver.com>
2020-12-17 18:13:27 +0900

    commit

--- src/main/java/com/example/demo/database/jpa/Repository/AttendanceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.database.jpa.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.database.jpa.DTO.*;
-
-public interface AttendanceRepository extends JpaRepository<AttendanceDTO, Long> {
-
-}
--- src/main/java/com/example/demo/database/jpa/Repository/departmentInter.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.database.jpa.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.database.jpa.DTO.*;
-public interface departmentInter extends JpaRepository<departmentDTO, Long> {
-
-}
